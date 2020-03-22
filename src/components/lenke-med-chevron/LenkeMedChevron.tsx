1393035d81036ed83d04c2f04a6f2e17edde00af
anders <anders@nomerstad.com>
2020-03-23 00:14:03 +0100

    Ikon for eksterne lenker

--- src/components/lenke-med-chevron/LenkeMedChevron.tsx
+++ src/components/lenke-med-chevron/LenkeMedChevron.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Lenke from "nav-frontend-lenker";
 import { HoyreChevron } from "nav-frontend-chevron";
+import eksternIkon from "../../assets/arrow-right.svg";
 
 type Props = {
   href: string;
@@ -10,21 +11,25 @@ type Props = {
   id?: string;
 };
 
-const lenkeTekstMedChevron = (tekst: React.ReactNode) => (
+const lenkeTekstMedChevron = (tekst: React.ReactNode, isExternal = false) => (
   <>
     <div><HoyreChevron className={"chevronlenke__chevron"} /></div>
-    {tekst}
+    <div className={"chevronlenke__tekst"}>
+      {tekst}
+      {isExternal && <img src={eksternIkon} alt={""} className={"chevronlenke__ekstern-ikon"} />}
+    </div>
   </>
 );
 
 const LenkeMedChevron = (props: Props) => {
   const { href, className, id, onClick, children } = props;
-  const lenkeTekst = lenkeTekstMedChevron(children);
+  const isExternal = !href.includes("nav.no");
+  const lenkeTekst = lenkeTekstMedChevron(children, isExternal);
 
   return (
     <Lenke
       href={href}
-      className={`chevronlenke ${className}`}
+      className={`chevronlenke${className ? ` ${className}` : ''}`}
       onClick={onClick}
       id={id}
     >
