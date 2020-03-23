27c1ed8b474810c25a895ac5e74f2dffcde82056
Morten Jansrud <morten.jansrud@nav.no>
2020-03-23 10:56:30 +0100

    Legg til pageTitle og metaDescription

--- src/utils/sanity/endpoints/contexts.ts
+++ src/utils/sanity/endpoints/contexts.ts
@@ -2,7 +2,7 @@ import { LocaleString, SanityLinkList } from "../serializers";
 
 export type RoleContext = {
   context: LocaleString;
-  order: number
+  order: number;
   description?: SanityLinkList[];
 };
 
