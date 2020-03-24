f238cde4864fbc532873ef38e73fafecbccc18c7
anders <anders@nomerstad.com>
2020-03-24 13:22:52 +0100

    Prøver på nytt

--- src/utils/StorageSafariFix.ts
+++ src/utils/StorageSafariFix.ts
@@ -1,29 +1,30 @@
 
 export const fixLocalStorageForSafari = () => {
-  if (!sessionStorage.setItem.bind || !localStorage.setItem.bind) {
-    return
+  try {
+    const sessionSetItemReal = sessionStorage.setItem.bind(sessionStorage);
+    const localSetItemReal = localStorage.setItem.bind(localStorage);
+
+    sessionStorage.setItem = (key: string, value: string) => {
+      try {
+        sessionSetItemReal(key, value);
+        console.log("test1");
+      }
+      catch (error) {
+        console.log(error);
+      }
+    };
+
+    localStorage.setItem = (key: string, value: string) => {
+      try {
+        localSetItemReal(key, value);
+        console.log("test2");
+      }
+      catch (error) {
+        console.log(error);
+      }
+    };
   }
-
-  const sessionSetItemReal = sessionStorage.setItem.bind(sessionStorage);
-  const localSetItemReal = localStorage.setItem.bind(localStorage);
-
-  sessionStorage.setItem = (key: string, value: string) => {
-    try {
-      sessionSetItemReal(key, value);
-      console.log("test1");
-    }
-    catch (error) {
-      console.log(error);
-    }
-  };
-
-  localStorage.setItem = (key: string, value: string) => {
-    try {
-      localSetItemReal(key, value);
-      console.log("test2");
-    }
-    catch (error) {
-      console.log(error);
-    }
-  };
+  catch (error) {
+    console.log(error);
+  }
 };
