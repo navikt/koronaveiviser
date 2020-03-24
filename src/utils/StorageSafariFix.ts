22378793e515c7c66dc76837d5ee715a2f4f5f4e
anders <anders@nomerstad.com>
2020-03-24 13:14:31 +0100

    Prøver på nytt

--- src/utils/StorageSafariFix.ts
+++ src/utils/StorageSafariFix.ts
@@ -1,23 +1,29 @@
 
 export const fixLocalStorageForSafari = () => {
+  if (!sessionStorage.setItem.bind || !localStorage.setItem.bind) {
+    return
+  }
+
   const sessionSetItemReal = sessionStorage.setItem.bind(sessionStorage);
   const localSetItemReal = localStorage.setItem.bind(localStorage);
 
   sessionStorage.setItem = (key: string, value: string) => {
     try {
       sessionSetItemReal(key, value);
+      console.log("test1");
     }
     catch (error) {
-      console.error(error);
+      console.log(error);
     }
   };
 
   localStorage.setItem = (key: string, value: string) => {
     try {
       localSetItemReal(key, value);
+      console.log("test2");
     }
     catch (error) {
-      console.error(error);
+      console.log(error);
     }
   };
 };
