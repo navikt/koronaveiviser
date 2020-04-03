36a50c6bb029fafa3b6c9dce075101c37f930ddf
anders <anders@nomerstad.com>
2020-04-03 16:49:35 +0200

    Justeringer på markering av ekstern lenke

--- src/components/lenke-med-chevron/LenkeMedChevron.tsx
+++ src/components/lenke-med-chevron/LenkeMedChevron.tsx
@@ -11,6 +11,8 @@ type Props = {
   id?: string;
 };
 
+const cssPrefix = "chevronlenke";
+
 const getHostname = (url: string) => url
   .replace(/https?:\/\//i, "")
   .replace(/www\./i, "")
@@ -18,14 +20,14 @@ const getHostname = (url: string) => url
 
 const lenkeTekstMedChevron = (tekst: React.ReactNode, externalHostname?: string) => (
   <>
-    <div><HoyreChevron className={"chevronlenke__chevron"} /></div>
+    <div><HoyreChevron className={`${cssPrefix}__chevron`} /></div>
     <div>
       {tekst}
       {externalHostname && (
-        <>
-          <span className={"chevronlenke__ekstern-ikon"}><ExternalLinkIcon /></span>
-          {`[${externalHostname}]`}
-        </>
+        <span className={`${cssPrefix}__ekstern`}>
+          <span className={`${cssPrefix}__ekstern-ikon`}><ExternalLinkIcon /></span>
+          {`(${externalHostname})`}
+        </span>
       )}
     </div>
   </>
@@ -39,7 +41,7 @@ const LenkeMedChevron = (props: Props) => {
   return (
     <Lenke
       href={href}
-      className={`chevronlenke${className ? ` ${className}` : ''}`}
+      className={`${cssPrefix}${className ? ` ${className}` : ''}`}
       onClick={onClick}
       id={id}
     >
